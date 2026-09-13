4286419de82ffa887c93398837f90022f9b81fa5
suchaildeveloppement-ship-it <suchaildeveloppement@gmail.com>
2026-09-13 15:34:00 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -7,8 +7,8 @@ export const config = {
 export function middleware(req) {
   const auth = req.headers.get("authorization");
 
-  const user = process.env.romaindevun;
-  const pass = process.env.123romaindevun;
+  const user = process.env.HUB_USER;
+  const pass = process.env.HUB_PASS;
 
   if (auth) {
     const [scheme, encoded] = auth.split(" ");
